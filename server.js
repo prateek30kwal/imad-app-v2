70a0baea7c3279b4004269bed08730ae8f4b4b89
prateek30kwal <prateek30.kwal@gmail.com>
2017-02-16 12:07:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req, res){
-    res.send('Hello ARTICLE-1 is requested');
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two',function(req, res){
  res.sendFile("Hello ARTICLE-2 is requested");
